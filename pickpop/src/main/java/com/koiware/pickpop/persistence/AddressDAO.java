db1f4a71c9b3ee061f0841f4ddf13d6c5f1a9b39
jslee <jslee@jslee-PC>
2016-01-25 18:21:46 +0900

    first at new notebook

--- pickpop/src/main/java/com/koiware/pickpop/persistence/AddressDAO.java
+++ pickpop/src/main/java/com/koiware/pickpop/persistence/AddressDAO.java
@@ -1,8 +1,14 @@
 package com.koiware.pickpop.persistence;
 
+import java.util.List;
+
 import com.koiware.pickpop.domain.AddressVO;
+import com.koiware.pickpop.domain.CityVO;
 
 public interface AddressDAO {
-	public void insertAddress(AddressVO vo);
+	public int insertAddress(AddressVO vo);
+	public AddressVO getAddress(int idAddress);
+	public List<CityVO> listCity();
+	public CityVO getCity(int idcity);
 	
 }
--- pickpop/src/main/java/com/koiware/pickpop/persistence/AddressDAOImpl.java
+++ pickpop/src/main/java/com/koiware/pickpop/persistence/AddressDAOImpl.java
@@ -1,11 +1,14 @@
 package com.koiware.pickpop.persistence;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import com.koiware.pickpop.domain.AddressVO;
+import com.koiware.pickpop.domain.CityVO;
 
 @Repository
 public class AddressDAOImpl implements AddressDAO{
@@ -15,7 +18,23 @@ public class AddressDAOImpl implements AddressDAO{
 	private static final String namespace = "com.koiware.pickpop.mapper.addressMapper";
 	
 	@Override
-	public void insertAddress(AddressVO vo){
-		sqlSession.insert(namespace+".insertAddress", vo);
+	public int insertAddress(AddressVO vo){
+		return sqlSession.insert(namespace+".insertAddress", vo);
+	}
+
+	@Override
+	public AddressVO getAddress(int idaddress) {
+		AddressVO vo = sqlSession.selectOne(namespace+".getAddress", idaddress);
+		return vo;
+	}
+
+	@Override
+	public List<CityVO> listCity() {
+		return sqlSession.selectList(namespace+".listCity");
+	}
+
+	@Override
+	public CityVO getCity(int idcity) {
+		return sqlSession.selectOne(namespace+".getCity", idcity);
 	}
 }
