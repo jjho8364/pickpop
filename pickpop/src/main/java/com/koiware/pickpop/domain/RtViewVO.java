b60e94baf4698103c89f19b2d4197dad1396435c
jslee <jslee@jslee-PC>
2016-03-02 11:18:56 +0900

    please push

---
+++ pickpop/src/main/java/com/koiware/pickpop/domain/RtViewVO.java
@@ -0,0 +1,18 @@
+package com.koiware.pickpop.domain;
+
+public class RtViewVO {
+	int idrtview;
+	int idproduct;
+	public int getIdrtview() {
+		return idrtview;
+	}
+	public void setIdrtview(int idrtview) {
+		this.idrtview = idrtview;
+	}
+	public int getIdproduct() {
+		return idproduct;
+	}
+	public void setIdproduct(int idproduct) {
+		this.idproduct = idproduct;
+	}
+}
